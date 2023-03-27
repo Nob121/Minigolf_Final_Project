1f4d4f9c112849f774eedb46832bd0d9ba6ebb26
Nob121 <tahmidambrose.420@gmail.com>
2023-03-26 21:30:14 -0700

    Working on CameraFollow

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    [SerializeField] private Transform objectToFollow;
+    #region Variables
 
-    void Update()
+    private Vector3 offset;
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothTime;
+    private Vector3 currentVelocity = Vector3.zero;
+
+    private void Awake() => offset = transform.position - target.position;
+
+    private void LateUpdate()
     {
-        transform.position = objectToFollow.position + new Vector3(0, 5, -10);
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
     }
+
+    #endregion
 }
