26fbeb4f422457367bf6a22be440125eb30a73e2
Nob121 <tahmidambrose.420@gmail.com>
2023-04-11 11:49:16 -0700

    Worked on the blade

--- Assets/Scripts/BladeRotator.cs
+++ Assets/Scripts/BladeRotator.cs
@@ -4,20 +4,13 @@ using UnityEngine;
 
 public class BladeRotator : MonoBehaviour
 {
-    public float rotationSpeed = 7.0f; // speed at which the blades should rotate
-    private float rotationDuration = 1.0f; // duration of the rotation animation
+    private float rotationSpeed = 400.0f; // speed at which the blades should rotate
+    //private float rotationDuration = 1.0f; // duration of the rotation animation
 
-    void Start()
+    private void Update()
     {
-        iTween.RotateBy(gameObject, iTween.Hash(
-            "z", -1.0f, // rotate around the z-axis by 1 full rotation (360 degrees)
-            "time", rotationDuration,
-            "easetype", iTween.EaseType.easeInOutSine,
-            "looptype", iTween.LoopType.loop,
-            "delay", 0.0f
-        ));
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
     }
 
-    
 }
 
