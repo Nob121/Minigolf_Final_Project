11c177b314da7f64570c94679f8f41c6e133abf8
Nob121 <tahmidambrose.420@gmail.com>
2023-04-23 21:07:49 -0700

    Sound Button, Instruction Button Done

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -9,6 +9,11 @@ public class MenuManager : MonoBehaviour
 {
     public string player;
     [SerializeField] private TMP_InputField playerName;
+    [SerializeField] private Image soundOnImage;
+    [SerializeField] private Image soundOffImage;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private BasePopUp menuPopUp;
+    [SerializeField] private BasePopUp instructionPopUp;
     //[SerializeField] private Button startButton;
 
     public void StartButton()
@@ -18,5 +23,29 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene("Level");
     }
 
+    public void soundOnButton()
+    {
+        soundOnImage.gameObject.SetActive(false);
+        soundOffImage.gameObject.SetActive(true);
+        audioSource.Pause();
+    }
 
+    public void soundOffButton()
+    {
+        soundOffImage.gameObject.SetActive(false);
+        soundOnImage.gameObject.SetActive(true);
+        audioSource.UnPause();
+    }
+
+    public void instructionOnButton()
+    {
+        menuPopUp.Close();
+        instructionPopUp.Open();
+    }
+
+    public void backButton()
+    {
+        menuPopUp.Open();
+        instructionPopUp.Close();
+    }
 }
--- Assets/Scripts/OptionsPopUp.cs
+++ Assets/Scripts/OptionsPopUp.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsPopUp : BasePopUp
 {
     [SerializeField] BasePopUp hitPopup;
     [SerializeField] BasePopUp parPopUp;
+
+    [SerializeField] private Image soundOnImage;
+    [SerializeField] private Image soundOffImage;
+    [SerializeField] private Image backGround;
     //[SerializeField] private AudioClip pauseMusic;
     private AudioSource audioSource;
     // Start is called before the first frame update
     private void Start()
     {
+        backGround.gameObject.SetActive(true);
+        soundOnImage.gameObject.SetActive(true);
         audioSource = GetComponent<AudioSource>();
         Time.timeScale = 0;
         audioSource.Play();
@@ -23,6 +31,9 @@ public class OptionsPopUp : BasePopUp
     }
     public void OnReturnToGameButton()
     {
+        backGround.gameObject.SetActive(false);
+        soundOnImage.gameObject.SetActive(false);
+        soundOffImage.gameObject.SetActive(false);
         audioSource.Pause();
         // Debug.Log("return to game");
         Time.timeScale = 1f;
@@ -30,4 +41,19 @@ public class OptionsPopUp : BasePopUp
         hitPopup.Open();
         parPopUp.Open();
     }
+
+    public void soundOnButton()
+    {
+        soundOnImage.gameObject.SetActive(false);
+        soundOffImage.gameObject.SetActive(true);
+        audioSource.Pause();
+    }
+
+    public void soundOffButton()
+    {
+        soundOffImage.gameObject.SetActive(false);
+        soundOnImage.gameObject.SetActive(true);
+        audioSource.UnPause();
+    }
+
 }
